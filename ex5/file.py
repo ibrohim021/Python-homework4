12cf90453947078e9121a9548b7d39a11ad33266
Ibrohim021 <dim_aviasky@mail.ru>
2022-10-09 17:23:21 +0300

    ex5

---
+++ ex5/file.py
@@ -0,0 +1,24 @@
+# Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+
+
+
+with open('ex5/poly_1.txt', 'w', encoding='utf-8') as file:
+    file.write('2*x^2 + 5*x^5')
+with open('ex5/poly_2.txt', 'w', encoding='utf-8') as file:
+    file.write('23*x^4 + 9*x^6')
+
+with open('ex5/poly_1.txt','r') as file:
+    poly_1 = file.readline()
+    list_of_poly_1 = poly_1.split()
+
+
+with open('ex5/poly_2.txt','r') as file:
+    poly_2 = file.readline()
+    list_of_poly_2 = poly_2.split()
+
+print(f'{list_of_poly_1} + {list_of_poly_2}')
+sum_poly = list_of_poly_1 + list_of_poly_2
+
+with open('ex5/sum_poly.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{list_of_poly_1} + {list_of_poly_2}')
